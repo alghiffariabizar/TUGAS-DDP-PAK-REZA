9e137cfe5f31422e367795938408b859faa50291
alghiffariabizar <145966068+alghiffariabizar@users.noreply.github.com>
2023-10-30 18:34:26 +0700

    TUGAS 5 SOAL 1&2

---
+++ tugas 5 soal 1&2.py
@@ -0,0 +1,14 @@
+# Nomor 1
+motor1 = ["B 3639 EMA","MIO",1000, "MERAH"]
+motor1.append("Rp.10.000.000")
+motor1.append(2)
+motor1.insert(2, "YAMAHA")
+motor1.insert(3, "MOTOR MATIC")
+print("Kode Kendaraan  : ", motor1[0])
+print("Nama Kendaraan  : ", motor1[1])
+print("Merk Kendaraan  : ", motor1[2])
+print("Jenis Kendaraan : ", motor1[3])
+print("CC Kendaraan    : ", motor1[4])
+print("Warna Kendaraan : ", motor1[5])
+print("harga           : ", motor1[6])
+print("Jumlah Ban      : ", motor1[7])
